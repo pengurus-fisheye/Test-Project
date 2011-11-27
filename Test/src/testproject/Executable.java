bcbf2d2e9899d0a466025e87d78fb7f046444292
Jakub Ocwieja <netkuba@students.mimuw.edu.pl>
2011-11-27 15:36:41 +0100

    [TEST-7] Create another method in Executable class

--- Test/src/testproject/Executable.java
+++ Test/src/testproject/Executable.java
@@ -4,6 +4,11 @@ public class Executable {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println(getBye());
 		return;
+	}
+	
+	private static String getBye() {
+		return "Bye";
 	}
 }
